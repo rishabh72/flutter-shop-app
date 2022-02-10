c20d54877447df2af57578b001ab52406d0a13d0
rishabh <sonirishabh018@gmail.com>
2022-02-10 16:46:37 +0530

    updated readme

---
+++ README.md
@@ -0,0 +1,27 @@
+## Flutter Shop App
+
+<br/>
+<br/>
+
+In this app, I created three screens-
+<br/>
+
+- ProductsList Screen
+- Favorites Screen
+- Cart Screen
+  I used provider package for state management and everything is very similiar to myntra app.
+  <br/>
+
+![Alt text](assets/imgs/pl1.jpeg 'products list')
+<br/>
+![Alt text](assets/imgs/pl2.jpeg 'mark favorites products list')
+<br/>
+
+![Alt text](assets/imgs/wl.jpeg 'wishlist')
+<br/>
+
+![Alt text](assets/imgs/c1.jpeg 'cart')
+<br/>
+
+![Alt text](assets/imgs/c2.jpeg 'cart popup')
+<br/>
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -14,39 +14,39 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final cartItems = Provider.of<Cart>(context).cartItems;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        iconTheme: IconThemeData(color: Colors.black),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(WishlistScreen.routeName);
-            },
-            icon: Icon(Icons.favorite_border_rounded),
-          )
-        ],
-      ),
-      body: cartItems.length > 0
-          ? Column(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Color.fromRGBO(240, 240, 240, 1),
-                    child: ListView.builder(
-                      itemBuilder: (ctx, index) {
-                        final itemKey = cartItems.keys.elementAt(index);
-                        final item = cartItems[itemKey] as CartItem;
-                        return CartListItem(item);
-                      },
-                      itemCount: cartItems.length,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: const IconThemeData(color: Colors.black),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(WishlistScreen.routeName);
+              },
+              icon: const Icon(Icons.favorite_border_rounded),
+            )
+          ],
+        ),
+        body: cartItems.isEmpty
+            ? const EmptyCart(
+                'https://constant.myntassets.com/checkout/assets/img/empty-bag.webp',
+              )
+            : Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: const Color.fromRGBO(240, 240, 240, 1),
+                      child: ListView.builder(
+                        itemBuilder: (ctx, index) {
+                          final itemKey = cartItems.keys.elementAt(index);
+                          final item = cartItems[itemKey] as CartItem;
+                          return CartListItem(item);
+                        },
+                        itemCount: cartItems.length,
+                      ),
                     ),
                   ),
-                ),
-                // BottomOrderBtnBox(cartItems.length),
-              ],
-            )
-          : EmptyCart(
-              'https://constant.myntassets.com/checkout/assets/img/empty-bag.webp'),
-    );
+                  BottomOrderBtnBox(cartItems.length),
+                ],
+              ));
   }
 }
